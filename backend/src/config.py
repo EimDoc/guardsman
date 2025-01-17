8e7b9de56dacbaaf6e206d491d0360337defc321
Efim Docenko <104095998+EimDoc@users.noreply.github.com>
2025-01-17 10:20:43 +0300

    added path to usb directory from .env

---
+++ backend/src/config.py
@@ -0,0 +1,3 @@
+from decouple import config
+
+PATH_TO_USB = config("PATH_TO_USB")
--- backend/src/utils/utils.py
+++ backend/src/utils/utils.py
@@ -1,17 +1,17 @@
 import glob
 import os
+from src.config import PATH_TO_USB
 
 
 def get_path_to_usb_drive():
     usb_drives = []
     out = []
-    for i in os.listdir("/Volumes"):
+    for i in os.listdir(PATH_TO_USB):
         if i != "Macintosh HD":
             usb_drives.append(i)
 
     for i in usb_drives:
-        for j in glob.glob(f"/Volumes/{i}/**", recursive=True):
+        for j in glob.glob(f"{PATH_TO_USB}/{i}/**", recursive=True):
             out.append(j)
 
     return out
-
